2916adf1cde0b2ea9cb413d1fd04ba8c6ee66af7
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-09-22 14:40:41 +0100

    Messed around trying to get edge detection to work on input button

--- src/TinySound.ino
+++ src/TinySound.ino
@@ -2,9 +2,13 @@
 
 #define adc_disable()  (ADCSRA &= ~(1<<ADEN))
 #define adc_enable() (ADCSRA |= (1<<ADEN) )
-#define BUTTON_PIN 3
+#define BUTTON_PIN PB2
+#define HD1 PB1
+#define HD2 PB4
+#define DELAY_CNT 11
 
 #include <avr/pgmspace.h>
+#include <avr/wdt.h>
 #include "wav.h"
 #include <avr/sleep.h>
 
@@ -13,24 +17,47 @@ volatile unsigned int song = 0;
 unsigned long lastInteruptTime = 0;
 volatile unsigned long timerCount = 0;
 
+volatile bool buttonEdge = false;
+
 void setup() {
-  
-  setupTimer();  
+
+  setupTimer();
   setOutputAndInterupts();
 }
 
 void loop() {
+  if( buttonEdge ){
+    buttonEdge = false;
+    nextSong();
+    /*
+    if( digitalRead(BUTTON_PIN)==HIGH ) {
 
+    }
+    */
+  }
+}
+
+void nextSong(){
+  if( ((timerCount-lastInteruptTime)>400) ){
+    song++;
+    lastInteruptTime = timerCount;
+    if( song > 1 ){
+      song = 0;
+      p = 0;
+      sleep();
+      return;
+    }
+  }
 }
 
 // Sample interrupt
-ISR(TIMER0_COMPA_vect) {  
+ISR(TIMER0_COMPA_vect) {
   unsigned long t = p;
-  unsigned long anHour = 28800000;
+  //unsigned long anHour = 28800000;
   timerCount++;
-  
-  //char sample = pgm_read_byte(&wav[p]); 
-  //char sample = ((t>>6)|t|t>>(t>>16))*10+((t>>11)&7);  
+
+  //char sample = pgm_read_byte(&wav[p]);
+  //char sample = ((t>>6)|t|t>>(t>>16))*10+((t>>11)&7);
   //char sample = ((1-(((t+10)>>((t>>9)&15))&2))*2)*((((t)>>10)^((t+20)>>10))&1)*32+(((t&4095)-2047)*((t/((t>>10&3)+1))&((t>>10&7)+5))+(t>>(((t>>12)+16)&25)&1)*t%512*(t%256-128)/2)/1024+128;
   //char sample = t>>6^t&0x25|t+(t^t>>11)-t* ((t%24?2:6)&t>>11)^t<<1&(t&0x256?t>>4:t>>10);
   //char sample = ( t* (( t>>9| t>>13 ) & 15)) & 129;
@@ -41,33 +68,21 @@ ISR(TIMER0_COMPA_vect) {
     sample = (t&(t>>6)+(t<<((t>>11)^((t>>13)+3L))|((t>>14%64L)+(t>>14)))&(-t>>5));
     break;
     case 1:
-    sample = ((t>>9)&(t^(-t>>8)+t)+20L*(t>>17)%256L)>>1;  
+    sample = ((t>>9)&(t^(-t>>8)+t)+20L*(t>>17)%256L)>>1;
+    break;
+    case 2:
+    sample = t;
     break;
   }
   p++;
   OCR1A = sample; OCR1B = sample ^ 255;
-  // End of data? Go to sleep
-  if (p == anHour) {
-    adc_disable();
-    sleep_enable();
-    sleep_cpu();  // 1uA
-  }
+
 }
 
-ISR(PCINT0_vect)
-{    
+ISR(INT0_vect)
+{
     //(PINB&(1<<BUTTON_PIN))!=0
-    if( ((timerCount-lastInteruptTime)>400) && digitalRead(BUTTON_PIN)==HIGH ){
-      song++;
-      
-      if( song > 1 ){
-        song = 0;
-        p = 0;
-        sleep();
-        return;        
-      }
-      lastInteruptTime = timerCount;
-    }
+    buttonEdge = true;
 
 }
 
@@ -75,9 +90,9 @@ void setupTimer(){
 
   lastInteruptTime= 0;
   timerCount = 0;
-  
+
   // Enable 64 MHz PLL and use as source for Timer1
-  PLLCSR = 1<<PCKE | 1<<PLLE;     
+  PLLCSR = 1<<PCKE | 1<<PLLE;
   // Set up Timer/Counter1 for PWM output
   TIMSK = 0;                              // Timer interrupts OFF
   TCCR1 = 1<<PWM1A | 2<<COM1A0 | 1<<CS10; // PWM A, clear on match, 1:1 prescale
@@ -92,35 +107,61 @@ void setupTimer(){
 }
 
 void setOutputAndInterupts(){
-  pinMode(4, OUTPUT);
-  pinMode(1, OUTPUT);
+  pinMode(HD1, OUTPUT);
+  pinMode(HD2, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
   //DDRB |= B010010;
+  adc_disable();
 
-  
   //DDRB |= B000100;
   //PORTB &= ~B000100;
-  
-  DDRB &= ~(1<<BUTTON_PIN);  
-  PORTB |= 1<<BUTTON_PIN;
 
-  GIMSK = 0b00100000;    // turns on pin change interrupts
-  PCMSK = 0b00001000;    // turn on interrupts on pins PB0, PB1, &amp;amp; PB4
-  
+  DDRB &= ~_BV(BUTTON_PIN);
+  PORTB |= _BV(BUTTON_PIN);
+  MCUCR = _BV(ISC01) | _BV(ISC00);
+
+  GIMSK = 0b01000000;    // turns on pin change interrupts
+  PCMSK = 0b00000000;    // turn on interrupts on pins PB3
+
   sei();                 // enables interrupts
 }
 
 void sleep()
 {
   TIMSK = 0;
+  digitalWrite(1,LOW);
+  digitalWrite(4,LOW);
   PORTB = _BV(BUTTON_PIN);
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  adc_disable();
+  //set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  GIMSK = 0b00100000;
+  PCMSK = 0b00000100;
   sleep_enable();
   sei();
+  wdt_disable();
+  delay_ten_us(1000);
+  MCUCR = _BV(SM1) |  _BV(SE);
   sleep_cpu();  // 1uA
 
-  sleep_disable();
+  //sleep_disable();
+
   setupTimer();
   setOutputAndInterupts();
+}
+
+// This function delays the specified number of 10 microseconds
+// it is 'hardcoded' and is calibrated by adjusting DELAY_CNT
+// in main.h Unless you are changing the crystal from 8mhz, dont
+// mess with this.
+void delay_ten_us(uint16_t us) {
+  uint8_t timer;
+  while (us != 0) {
+    // for 8MHz we want to delay 80 cycles per 10 microseconds
+    // this code is tweaked to give about that amount.
+    for (timer=0; timer <= DELAY_CNT; timer++) {
+      __asm__("nop\n\t");
+      __asm__("nop\n\t");
+    }
+    __asm__("nop\n\t");
+    us--;
+  }
 }
